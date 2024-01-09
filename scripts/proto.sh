481c553d5f72814d748c1cfd65514ce1e7f942aa
Dennis Wethmar <denniswethmar@gmail.com>
2024-01-09 14:45:52 +0100

    Nice

--- scripts/proto.sh
+++ scripts/proto.sh
@@ -1,10 +1,2 @@
 #! /bin/bash
-
-set -e
-set -u
-
-rm -rf ./proto/v1/**/*.go
-
-docker build -t lingo-protobuf -f ./provision/protoc/Dockerfile .
-
-docker run --rm -v $(pwd)/proto:/proto lingo-protobuf
+docker run --volume "$(pwd):/workspace" --workdir /workspace bufbuild/buf generate -o proto
